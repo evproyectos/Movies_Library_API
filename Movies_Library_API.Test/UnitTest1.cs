7935e8e592f2b95117a1e373760e2c31d932300c
evproyectos <evproyectosp@gmail.com>
2023-09-06 20:08:05 -0600

    Project for tests createds

---
+++ Movies_Library_API.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Movies_Library_API.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
